e531de213cdba9fa6a6f681be38af6a2b9572a66
bstee615 <benjaminjsteenhoek@gmail.com>
2023-12-19 11:04:18 -0600

    Remove GUI for now to make the build lighting fast

--- src/bin/gui.rs
+++
@@ -1,6 +0,0 @@
-use blokus::gui;
-use gui::get_app;
-
-fn main() {
-    get_app().run();
-}
--- src/gui/globals.rs
+++
@@ -1,9 +0,0 @@
-use bevy::math::Vec2;
-
-pub const GRID_SQUARES: isize = 10;
-pub const GRID_SIZE: f32 = GRID_SQUARES as f32;
-pub const SQUARE_SIZE: f32 = 30.0;
-pub const PAD_SIZE: f32 = 5.0;
-pub const SQUARE_PLUS_PAD_SIZE: f32 = SQUARE_SIZE + PAD_SIZE;
-pub const BOARD_SIZE: f32 = GRID_SIZE * SQUARE_PLUS_PAD_SIZE;
-pub const BOARD_OFFSET: Vec2 = Vec2::new(0.0, 0.0);
